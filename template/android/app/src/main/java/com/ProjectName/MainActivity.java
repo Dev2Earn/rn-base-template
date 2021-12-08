1c15f9279234aeae1e41d22cc8e5694229b3d2ae
Phan Kiet <44119764+phankietit@users.noreply.github.com>
2021-12-08 23:25:02 +0700

    update project name

--- template/android/app/src/main/java/com/ProjectName/MainActivity.java
+++ template/android/app/src/main/java/com/ProjectName/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rnbase;
+package com.ProjectName;
 
 import android.os.Bundle;
 
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "rnBase";
+    return "ProjectName";
   }
 
   @Override
--- template/ios/ProjectNameTests/ProjectNameTests.m
+++ template/ios/ProjectNameTests/ProjectNameTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rnBaseTests : XCTestCase
+@interface ProjectNameTests : XCTestCase
 
 @end
 
-@implementation rnBaseTests
+@implementation ProjectNameTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
